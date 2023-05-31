04b65d699aa7a54144f5f022c95d27e20c942241
seongmin221 <seongmin221@naver.com>
2023-05-31 20:28:49 +0900

    renamed parser.py

---
+++ LRParser.py
@@ -0,0 +1,116 @@
+SLRTable = {
+  0 : {'vtype' : 's5', 'class': 's6', '$': 'r4', 'CODE': 1, 'VDECL' : 2, 'FDECL' : 3, 'CDECL': 4},
+  1 : {'$': 'acc'},
+  2 : {'vtype': 's5', 'class': 's6', '$': 'r4', 'CODE': 7, 'VDECL': 2, 'FDECL': 3, 'CDECL': 4},
+  3 : {'vtype': 's5', 'class': 's6', '$': 'r4', 'CODE': 8, 'VDECL': 2, 'FDECL': 3, 'CDECL': 4},
+  4 : {'vtype': 's5', 'class': 's6', '$': 'r4', 'CODE': 9, 'VDECL': 2, 'FDECL': 3, 'CDECL': 4},
+  5 : {'id': 's10', 'ASSIGN': 11},
+  6 : {'id': 's12'},
+  7 : {'$': 'r1'},
+  8 : {'$': 'r2'},
+  9 : {'$': 'r3'},
+  10 : {'semi': 's13', 'assign': 's15', 'lparen': 's14'},
+  11 : {'semi': 's16'},
+  12 : {'lbrace': 's17'},
+  13 : {'vtype': 'r5', 'id': 'r5', 'rbrace': 'r5', 'if': 'r5', 'while': 'r5', 'return': 'r5', 'class': 'r5', '$': 'r5'},
+  14 : {'vtype': 's19', 'rparen': 'r21', 'ARG': 18},
+  15 : {'id': 's28', 'literal': 's22', 'character': 's23', 'boolstr': 's24', 'lparen': 's27', 'num': 's29', 'RHS': 20, 'EXPR': 21, 'T': 25, 'F': 26},
+  16 : {'vtype': 'r6', 'id': 'r6', 'rbrace': 'r6', 'if': 'r6', 'while': 'r6', 'return': 'r6', 'class': 'r6', '$': 'r6'},
+  17 : {'vtype': 's5', 'rbrace': 'r38', 'VDECL': 31, 'FDECL': 32, 'ODECL': 30},
+  18 : {'rparen': 's33'},
+  19 : {'id': 's34'},
+  20 : {'semi': 'r7'},
+  21 : {'semi': 'r8', 'addsub': 's35'},
+  22 : {'semi': 'r9'},
+  23 : {'semi': 'r10'},
+  24 : {'semi': 'r11'},
+  25 : {'semi': 'r13', 'addsub': 'r13', 'multdiv': 's36', 'rparen': 'r13'},
+  26 : {'semi': 'r15', 'addsub': 'r15', 'multdiv': 'r15', 'rparen': 'r15'},
+  27 : {'id': 's28', 'lparen': 's27', 'num': 's29', 'RHS': 37, 'T': 25, 'F': 26},
+  28 : {'semi': 'r17', 'addsub': 'r17', 'multdiv': 'r17', 'rparen': 'r17'},
+  29 : {'semi': 'r18', 'addsub': 'r18', 'multdiv': 'r18', 'rparen': 'r18'},
+  30 : {'rbrace': 's38'},
+  31 : {'vtype': 's5', 'rbrace': 'r38', 'VDECL': 31, 'FDECL': 32, 'ODECL': 39},
+  32 : {'vtype': 's5', 'rbrace': 'r38', 'VDECL': 31, 'FDECL': 32, 'ODECL': 40},
+  33 : {'lbrace': 's41'},
+  34 : {'rparen': 'r23', 'comma': 's43', 'MOREARGS': 42},
+  35 : {'id': 's28', 'lparen': 's27', 'num': 's29', 'T': 44, 'F': 26},
+  36 : {'id': 's28', 'lparen': 's27', 'num': 's29', 'F': 45},
+  37 : {'addsub': 's35', 'rparen': 's46'},
+  38 : {'vtype': 'r35', 'class': 'r35', '$': 'r35'},
+  39 : {'rbrace': 'r36'},
+  40 : {'rbrace': 'r37'},
+  41 : {'vtype': 's53', 'id': 's54', 'rbrace': 'r25', 'if': 's51', 'while': 's52', 'return': 'r25', 'VDECL': 49, 'ASSIGN': 50, 'BLOCK': 47, 'STMT': 48},
+  42 : {'rparen': 'r29'},
+  43 : {'vtype': 's55'},
+  44 : {'semi': 'r12', 'addsub': 'r12', 'multdiv': 's36', 'rparen': 'r12'},
+  45 : {'semi': 'r14', 'addsub': 'r14', 'multdiv': 'r14', 'rparen': 'r14'},
+  46 : {'semi': 'r16', 'addsub': 'r16', 'multdiv': 'r16', 'rparen': 'r16'},
+  47 : {'return': 's57', 'RETURN': 56},
+  48 : {'vtype': 's53', 'id': 's54', 'rbrace': 'r25', 'if': 's51', 'while': 's52', 'return': 'r25', 'VDECL': 49, 'ASSIGN': 50, 'BLOCK': 58, 'STMT': 48},
+  49 : {'vtype': 'r26', 'id': 'r26', 'rbrace': 'r26', 'if': 'r26', 'while': 'r26', 'return': 'r26'},
+  50 : {'semi': 's59'},
+  51 : {'lparen': 's60'},
+  52 : {'lparen': 's61'},
+  53 : {'id': 's62', 'ASSIGN': 11},
+  54 : {'assign': 's15'},
+  55 : {'id': 's63'},
+  56 : {'rbrace': 's64'},
+  57 : {'id': 's28', 'literal': 's22', 'character': 's23', 'boolstr': 's24', 'lparen': 's27', 'num': 's29', 'RHS': 65, 'EXPR': 21, 'T': 25, 'F': 26},
+  58 : {'rbrace': 'r24', 'return': 'r24'},
+  59 : {'vtype': 'r27', 'id': 'r27', 'rbrace': 'r27', 'if': 'r27', 'while': 'r27', 'return': 'r27'},
+  60 : {'boolstr': 's67', 'COND': 66},
+  61 : {'boolstr': 's67', 'COND': 68},
+  62 : {'semi': 's13', 'assign': 's15'},
+  63 : {'rparen': 'r23', 'comma': 's43', 'MOREARGS': 69},
+  64 : {'vtype': 'r19', 'rbrace': 'r19', 'class': 'r19', '$': 'r19'},
+  65 : {'semi': 's70'},
+  66 : {'rparen': 's71'},
+  67 : {'rparen': 'r31', 'comp': 's72'},
+  68 : {'rparen': 's73'},
+  69 : {'rparen': 'r22'},
+  70 : {'rbrace': 'r34'},
+  71 : {'lbrace': 's74'},
+  72 : {'boolstr': 's67', 'COND': 75},
+  73 : {'lbrace': 's76'},
+  74 : {'vtype': 's53', 'id': 's54', 'rbrace': 'r25', 'if': 's51', 'while': 's52', 'return': 'r25', 'VDECL': 49, 'ASSIGN': 50, 'BLOCK': 77, 'STMT': 48},
+  75 : {'rparen': 'r30'},
+  76 : {'vtype': 's53', 'id': 's54', 'rbrace': 'r25', 'if': 's51', 'while': 's52', 'return': 'r25', 'VDECL': 49, 'ASSIGN': 50, 'BLOCK': 78, 'STMT': 48},
+  77 : {'rbrace': 's79'},
+  78 : {'rbrace': 's80'},
+  79 : {'vtype': 'r33', 'id': 'r33', 'rbrace': 'r33', 'if': 'r33', 'while': 'r33', 'else': 's82', 'return': 'r33', 'ELSE': 81},
+  80 : {'vtype': 'r29', 'id': 'r29', 'rbrace': 'r29', 'if': 'r29', 'while': 'r29', 'return': 'r29'},
+  81 : {'vtype': 'r28', 'id': 'r28', 'rbrace': 'r28', 'if': 'r28', 'while': 'r28', 'return': 'r28'},
+  82 : {'lbrace': 's83'},
+  83 : {'vtype': 's53', 'id': 's54', 'rbrace': 'r25', 'if': 's51', 'while': '52', 'return': 'r25', 'VDECL': 49, 'ASSIGN': 50, 'BLOCK': 84, 'STMT': 48},
+  84 : {'rbrace': 's85'},
+  85 : {'vtype': 'r32', 'id': 'r32', 'rbrace': 'r32', 'if': 'r32', 'while': 'r32'}
+}
+
+inputString = "class id lbrace rbrace"
+inputSequence = inputString.split(' ')
+stack = []
+
+index = 0
+state = 0
+
+while index < len(inputSequence):
+  try:
+    print("state :", state, ", Next Input Symbol :", inputSequence[index], ", Decision", SLRTable[state][inputSequence[index]])
+    
+    if SLRTable[state][inputSequence[index]].__contains__("s"):
+      nextState = int(SLRTable[state][inputSequence[index]].strip("s"))
+      # 다음 state를 stack 의 top에 push
+      stack.append(nextState)
+      index += 1
+
+    elif SLRTable[state][inputSequence[index]].__contains__("r"):
+      stack.pop()
+      
+
+    else:
+      print("else")
+      index += 1
+
+  except Exception as e:
+    print("error with parsing", e) 
